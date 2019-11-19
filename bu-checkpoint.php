f877721b504379d5e3ca2c09d120a70f78dad7d5
Jim Reevior <jim@hirozed.com>
2019-11-19 11:32:58 -0500

    Plugin base from scaffold

---
+++ bu-checkpoint.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     BU Checkpoint
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     bu-checkpoint
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Bu_Checkpoint
+ */
+
+// Your code starts here.
