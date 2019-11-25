7a7274337457ba954e3b2cc3a51dd8e310dca9f5
Jim Reevior <jim@hirozed.com>
2019-11-24 19:52:22 -0500

    React enqueued, updates to grunt and npm for script and style handling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function( grunt ) {
 
 	'use strict';
+	// Require external packages.
+	const autoprefixer = require( 'autoprefixer' );
+	const sass = require( 'node-sass' );
 
 	// Project configuration
 	grunt.initConfig( {
@@ -18,6 +21,116 @@ module.exports = function( grunt ) {
 				src: [ '*.php', '**/*.php', '!\.git/**/*', '!bin/**/*', '!node_modules/**/*', '!tests/**/*' ]
 			}
 		},
+		watch: {
+			grunt: {
+				files: [ 'Gruntfile.js' ],
+				options: {
+					reload: true,
+				},
+			},
+			scripts: {
+				files: [
+					'js-dev/**/*.js',
+				],
+				tasks: [ 'scripts' ],
+				options: {
+					spawn: false,
+				},
+			},
+			styles: {
+				files: [
+					'node_modules/responsive-foundation/css-dev/**/*.scss',
+					'css-dev/**/*.scss',
+				],
+				tasks: [ 'styles' ],
+				options: {
+					spawn: false,
+				},
+			},
+		},
+		browserify: {
+			options: {
+				watch: true,
+				browserifyOptions: {
+					debug: false,
+					transform: [ [ 'babelify' ] ],
+				},
+			},
+			dist: {
+				files: [
+					{
+						expand: true, // Enable dynamic expansion.
+						cwd: 'js-dev/', // Src matches are relative to this path.
+						src: [ '*.js' ], // Actual pattern(s) to match. Targets root JS files.
+						dest: 'js/', // Destination path prefix.
+					},
+				],
+			},
+		},
+		uglify: {
+			scripts: {
+				options: {
+					sourceMap: true,
+				},
+				expand: true,
+				cwd: 'js/',
+				src: [ '*.js' ],
+				dest: 'js/',
+			},
+			vendor: {
+				options: {
+					sourceMap: true,
+				},
+				expand: true,
+				cwd: 'js/vendor',
+				src: [ '*.js', '!*.min.js' ],
+				dest: 'js/vendor',
+			},
+		},
+		sass: {
+			options: {
+				outputStyle: 'compressed',
+				implementation: sass,
+				sourceMap: true,
+				indentType: 'space',
+				indentWidth: 2,
+				precision: '5',
+				includePaths: [
+					'node_modules/normalize-scss/sass',
+					'node_modules/mathsass/dist/',
+					'node_modules/responsive-foundation/css-dev',
+				],
+				bundleExec: true,
+			},
+			devl: {
+				options: {
+					outputStyle: 'expanded',
+				},
+				files: {
+					'style.css': 'css-dev/style.scss',
+					'ie.css': 'css-dev/ie.scss',
+				},
+			},
+			prod: {
+				files: {
+					'style.min.css': 'css-dev/style.scss',
+					'ie.min.css': 'css-dev/ie.scss',
+				},
+			},
+		},
+		postcss: {
+			defaults: {
+				options: {
+					map: {
+						inline: false, // Save all sourcemaps as separate files.
+					},
+					processors: [
+						autoprefixer, // add vendor prefixes.
+					],
+				},
+				src: ['style.css', 'style.min.css'],
+			},
+		},
 
 		wp_readme_to_markdown: {
 			your_target: {
@@ -31,7 +144,7 @@ module.exports = function( grunt ) {
 			target: {
 				options: {
 					domainPath: '/languages',
-					exclude: [ '\.git/*', 'bin/*', 'node_modules/*', 'tests/*' ],
+					exclude: ['\.git/*', 'bin/*', 'node_modules/*', 'tests/*'],
 					mainFile: 'bu-checkpoint.php',
 					potFilename: 'bu-checkpoint.pot',
 					potHeaders: {
@@ -43,14 +156,40 @@ module.exports = function( grunt ) {
 				}
 			}
 		},
+		clean: {
+			languages: [ 'languages/*' ],
+			js: [ 'js/**/*.js', 'js/**/*.map' ],
+		},
+		sasslint: {
+			target: 'css-dev/**/*.scss',
+			// see .sasslintrc for options.
+		},
 	} );
 
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
-	grunt.registerTask( 'default', [ 'i18n','readme' ] );
-	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
-	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
+	grunt.loadNpmTasks( 'grunt-browserify' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-postcss' );
+	grunt.loadNpmTasks( 'grunt-sass' );
+	grunt.loadNpmTasks( 'grunt-notify' );
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-sass-lint' );
 
 	grunt.util.linefeed = '\n';
+
+	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
+	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
+	grunt.registerTask( 'styles', [ 'sass', 'postcss' ] );
+	grunt.registerTask( 'scripts', [
+		'clean:js',
+		'browserify',
+		'uglify',
+	] );
+	grunt.registerTask( 'build', [ 'styles', 'scripts', 'i18n' ] );
+	grunt.registerTask( 'default', [ 'watch' ] );
 
 };
--- bu-checkpoint.php
+++ bu-checkpoint.php
@@ -13,9 +13,45 @@
 
 namespace BU\Plugins\Checkpoint;
 
-define( BU_CHECKPOINT_CPT, 'bu-checkpoint' );
-define( BU_CHECKPOINT_STATUS, 'bu-checkpoint-status' );
-define( BU_CHECKPINT_STAGES, 'bu-checkpoint-stages' );
+define( 'BU_CHECKPOINT_VER', '1.0' );
 
+// CPT & Taxonomies
+define( 'BU_CHECKPOINT_CPT', 'bu-checkpoint' );
+define( 'BU_CHECKPOINT_STATUS', 'bu-checkpoint-status' );
+define( 'BU_CHECKPOINT_STAGES', 'bu-checkpoint-stages' );
+
+// Required
 require 'inc/taxonomy.php';
 require 'inc/post-type.php';
+
+/*
+* Adds a submenu to the Settings menu.
+* the page will contain the ID of html element that hosts our JSX components.
+*/
+function create_admin_menu() {
+	add_menu_page( 'Checkpoint', 'Checkpoint', 'edit_others_pages', 'bu-checkpoint', __NAMESPACE__ . '\\display_admin_screen', 'dashicons-yes' );
+}
+add_action( 'admin_menu', __NAMESPACE__ . '\\create_admin_menu' );
+
+function display_admin_screen() {
+	echo '<div id="main"></div>';
+}
+
+/**
+ * Enqueue React & scripts
+ */
+function enqueue_scripts() {
+	if ( ! in_array( BU_ENVIRONMENT_TYPE, array( 'test', 'prod' ) ) ) {
+		// Load Dev scripts in sandbox
+		wp_enqueue_script( 'checkpoint-react', 'https://unpkg.com/react@16/umd/react.development.js', array(), BU_CHECKPOINT_VER, true );
+		wp_enqueue_script( 'checkpoint-react-dom', 'https://unpkg.com/react-dom@16/umd/react-dom.development.js', array( 'checkpoint-react' ), BU_CHECKPOINT_VER, true );
+	} else {
+		// Load Prod scripts.
+		wp_enqueue_script( 'checkpoint-react', 'https://unpkg.com/react@16/umd/react.production.min.js', array(), BU_CHECKPOINT_VER, true );
+		wp_enqueue_script( 'checkpoint-react-dom', 'https://unpkg.com/react-dom@16/umd/react-dom.production.min.js', array( 'checkpoint-react' ), BU_CHECKPOINT_VER, true );
+	}
+
+	wp_enqueue_script( 'bu-checkpoint', \plugin_dir_url( __FILE__ ) . 'js/app.js', array( 'checkpoint-react', 'checkpoint-react-dom' ), BU_CHECKPOINT_VER, true );
+}
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
